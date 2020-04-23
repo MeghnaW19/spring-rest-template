32aac240a22a46142edd3f2d2868c72d881e7895
MeghnaW19 <meghnanvt70@gmail.com>
2020-04-23 21:05:47 +0530

    Update CovidStatServiceImplTest.java

--- src/test/java/com/stackroute/commander/test/service/CovidStatServiceImplTest.java
+++ src/test/java/com/stackroute/commander/test/service/CovidStatServiceImplTest.java
@@ -1,18 +1,9 @@
 package com.stackroute.commander.test.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stackroute.commander.test.domain.CovidStat;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
-import static org.junit.jupiter.api.Assertions.*;
 
 //@ExtendWith(MockitoExtension.class)
 ///**
